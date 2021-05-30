0ab75aa9d1560b6757c0127423e5afeb7904597e
Ben PH <benphawke@gmail.com>
2021-05-30 14:18:18 +0200

    Refine VSpace documentation

--- src/kernel_api/objects/vspace/arm/paging.rs
+++ src/kernel_api/objects/vspace/arm/paging.rs
@@ -1,21 +1,52 @@
 //! aarch64 stuff
+//!
+
+/// A capability used to create [ASIDPool] capabilities.
+///
+/// There is a fixed maximum number of applications the system can support. This capability is used to create [ASIDPool] capabilities, which themselves authorise the creation of a subset of these applications.
 
 pub struct ASIDControl;
-/// a 4k object, that can contain up to 1024 `VSpace`s
-pub struct ASIDPool;
-pub struct VMAttributes;
-pub struct PageGetAddress {
-    err: usize,
-    paddr: usize,
+use super::VSpace;
+
+/// Specifies the cache behaviour of a page being mapped
+pub enum VMAttributes {
+    /// A mappings data can be cached
+    PageCachable,
+    /// A mapping can be parity checked
+    ParityEnabled,
+    /// A mapping is non-executable
+    ExecuteNever,
 }
 
+/// The physical address of an underlying frame
+///
+/// TODO: change err into an `Option<...>`
+pub struct PageGetAddress {
+    pub err: usize,
+    pub paddr: usize,
+}
+
+/// Extracted contents of the IPC message when a thread triggers a page fault.
+///
+/// Replying to the fault IPC will restart the thread.
+/// TODO put this into thread stuff?
+pub struct VMFault {
+    pc_restart: usize,
+    addr: usize,
+    instruction_fault: bool,
+    fault_status_register: usize,
+}
 use crate::kernel_api::{
+    capability_space::{CapRights, Slot},
     objects::untyped_memory::Untyped,
-    objects::vspace::{ IOSpace, VSpace },
+    objects::vspace::IOSpace,
     CapSpace,
-    capability_space::{CapRights, Slot },
 };
 
+/// A 4k object, that can contain up to 1024 `VSpace`s
+///
+/// For a vspace to be usable by an application, it must be assigned to an `ASIDPool`
+pub struct ASIDPool;
 impl ASIDPool {
     /// Uses an `ASIDControl` and an `Untyped` representing a 4k memory object to make a new `ASIDPool`
     pub fn new(authority: &mut ASIDControl, untyped_4k: Untyped, cspace: CapSpace, slot: Slot) {
@@ -27,6 +58,7 @@ impl ASIDPool {
     }
 }
 
+/// Documentation not present for aarch64
 pub struct IOPageTable;
 impl IOPageTable {
     pub fn map(&mut self, iospace: IOSpace, addr: usize) {
@@ -36,23 +68,135 @@ impl IOPageTable {
         unimplemented!()
     }
 }
-
-pub struct Page;
-impl Page {
+pub trait CacheControl {
     /// cleans the data cache range given within this page out to RAM
-    pub fn clean_data(&mut self, start_offset: usize, end_offset_excl: usize) {
-        unimplemented!()
-    }
+    fn clean_data(&mut self, start_offset: usize, end_offset_excl: usize);
     /// cleans and invalidates the cache range given within this page and flushes data out to RAM
-    pub fn clean_invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize) {
-        unimplemented!()
-    }
+    fn clean_invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize);
     /// Invalidates the cache range within the page.
     /// The start and end should be alligned to a cache line boundary where possible
     /// If they are not aligned, an additional clean is done to the outer cache lines in full also
-    pub fn invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize) {
+    fn invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize);
+    fn unify_instructions(&mut self, start: usize, end: usize);
+}
+
+/// Level 0 in the 4-level page-table structure.
+///
+/// Provides a global VSpace, i.e. from 0x0 to 2^47 - 1
+pub struct PageGlobalDirectory;
+/// Level 1 mapping structure. Is effectively a sparse-array of up to 512
+/// entries, each being either a 1GiB frame (HugePage), or a PageDirectory.
+///
+/// An example structure to represent this is as follows
+/// ```
+/// enum Level2 {
+///     PageDirectory,
+///     HugePage,
+/// }
+/// struct PUDSpec {
+///     inner: [Option<Level2> ; 512]
+/// }
+/// ```
+///
+/// ```
+/// // Overview of mapping in address ranges 0xA0_0000_0000..=0xA0_001F_FFFF
+/// // and 0xA0_0040_0000..=0xA0_005F_FFFF
+/// // our test values
+/// let a_ptr: *mut u8 = 0xA0_0000_0000
+/// let b_ptr: *mut u8 = 0xA0_001F_FFFF
+/// let x_ptr: *mut u8 = 0xA0_0040_0000
+/// let y_ptr: *mut u8 = 0xA0_005F_FFFF
+/// // To start, no addresses can be mapped
+/// // Mapping in a PUD gives 0x80_000_0000
+/// // 0xA0_0000_0000..=0xA0_3FFF_FFFF
+///
+/// // this would cause a VMFault, as it's not yet mapped
+/// // let x_val = unsafe {*x};
+/// // First, make a PUD. The untyped it's coming from must be 12bits in size
+/// let mut pud = PageUpperDirectory::from_ut(
+///     &mut untyped,
+///     &cap_space,
+///     &mut target_cnode,
+///     &target_slot,
+/// );
+/// // Now, map it in so that we have 1GiB address space that can
+/// // take either PageDirectory mappings, or
+/// pud.map(vspace, 0xA0_0000_0000, VMAttributes::default())
+///
+/// let read_rights = RightsBuilder::new().can_read()..build();
+/// let write_rights = RightsBuilder::new().can_write().build();
+/// let rw_rights = RightsBuilder::new().can_read().can_write().build();
+/// // map it in to read
+/// ```
+pub struct PageUpperDirectory;
+/// Level 2 in the 4-level page-table structure.
+///
+/// Address bits 21..=29
+pub struct PageDirectory;
+/// Level 3 in the 4-level page-table structure.
+///
+/// Address bits 12..=20
+pub struct PageTable;
+
+impl CacheControl for PageGlobalDirectory {
+    fn clean_data(&mut self, start_offset: usize, end_offset_excl: usize) {
         unimplemented!()
     }
+    fn clean_invalidate_data(&mut self, start: usize, end: usize) {
+        unimplemented!()
+    }
+    fn invalidate_data(&mut self, start: usize, end: usize) {
+        unimplemented!()
+    }
+    fn unify_instructions(&mut self, start: usize, end: usize) {
+        unimplemented!()
+    }
+}
+/// For map/unmap of a paging object in/out of its host object
+pub trait Mapping {
+    type Host;
+    fn map(&mut self, host: Self::Host, vaddr: usize, attr: VMAttributes);
+    fn unmap(&self);
+}
+impl Mapping for PageUpperDirectory {
+    type Host = PageGlobalDirectory;
+    fn map(&mut self, pgd: Self::Host, vaddr: usize, attr: VMAttributes) {
+        unimplemented!()
+    }
+    fn unmap(&self) {
+        unimplemented!()
+    }
+}
+
+/// Corresponds to a frame of physical memory that is used to implement virtual memory pages in a virtual address space
+///
+/// The virtual address for a mapping must be alligned to page size, and must be mapped to a suitable vspace and all the intermediate paging structures required.
+/// To map a page readable and/or writable, th page capability must have read ond/or write permissions.
+/// If the permissions of the CapRights given to the mapping exceed tthe rights of the page being mapped, then the mapping permissions are silently down-graded.
+pub enum Page {
+    /// 1GiB
+    Huge,
+    /// 2MiB
+    Large,
+    /// 1KiB
+    Small,
+}
+impl CacheControl for Page {
+    fn clean_data(&mut self, start_offset: usize, end_offset_excl: usize) {
+        unimplemented!()
+    }
+    fn clean_invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize) {
+        unimplemented!()
+    }
+    fn invalidate_data(&mut self, start_offset: usize, end_offset_excl: usize) {
+        unimplemented!()
+    }
+    fn unify_instructions(&mut self, start_offset: usize, end_offset_excl: usize) {
+        unimplemented!()
+    }
+}
+
+impl Page {
     /// Get the physical address of the underlying frame
     pub fn get_address(&self) -> PageGetAddress {
         unimplemented!()
@@ -60,9 +204,12 @@ impl Page {
     pub fn map_io(&mut self, iospace: IOSpace, rights: CapRights, addr: usize) {
         unimplemented!()
     }
+
     /// Takes a VSpace capability, and installs a reference to this page in the lowest-level
     /// unmapped paging structure corresponding to vaddr. Fails with `FailedLookup` error if
     /// the required paging structures are not present
+    ///
+    /// To share a Page, copy its capability, and use this method on said copy to put it into the second address space.
     pub fn map(&mut self, vspace: VSpace, vaddr: usize, rights: CapRights, attr: VMAttributes) {
         unimplemented!()
     }
@@ -76,55 +223,25 @@ impl Page {
     pub fn re_map(&mut self, vspace: VSpace, rights: CapRights, attr: VMAttributes) {
         unimplemented!()
     }
-    pub fn unify_instructions(&mut self, start_offset: usize, end_offset_excl: usize) {
+}
+
+impl Mapping for PageTable {
+    type Host = VSpace;
+
+    fn map(&mut self, vspace: Self::Host, vaddr: usize, attr: VMAttributes) {
+        unimplemented!()
+    }
+    fn unmap(&self) {
         unimplemented!()
     }
 }
 
-pub struct PageTable;
-impl PageTable {
-    pub fn map(&mut self, vspace: VSpace, vaddr: usize, rights: CapRights, attr: VMAttributes) {
+impl Mapping for PageDirectory {
+    type Host = PageUpperDirectory;
+    fn map(&mut self, pud: Self::Host, vaddr: usize, attr: VMAttributes) {
         unimplemented!()
     }
-    pub fn unmap(&self) {
-        unimplemented!()
-    }
-}
-
-/// level 2 in the pageing structure
-pub struct PageDirectory;
-impl PageDirectory {
-    /// map a `PageDirectory` (level 2) to an `PageUpperDirectory` (level 1)
-    pub fn map(&mut self, pud: PageUpperDirectory, vaddr: usize, attr: VMAttributes) {
-        unimplemented!()
-    }
-    /// Unmaps this object from a level 1 `PageUpperDirectory`
-    pub fn unmap(&self) {
-        unimplemented!()
-    }
-}
-
-pub struct PageGlobalDirectory;
-impl PageGlobalDirectory {
-    pub fn clean_data(&mut self, start_offset: usize, end_offset_excl: usize) {
-        unimplemented!()
-    }
-    pub fn clean_invalidate_data(&mut self, start: usize, end: usize) {
-        unimplemented!()
-    }
-    pub fn invalidate_data(&mut self, start: usize, end: usize) {
-        unimplemented!()
-    }
-    pub fn unify_instruction(&mut self, start: usize, end: usize) {
-        unimplemented!()
-    }
-}
-pub struct PageUpperDirectory;
-impl PageUpperDirectory {
-    pub fn map(&mut self, pgd: PageGlobalDirectory, vaddr: usize, attr: VMAttributes) {
-        unimplemented!()
-    }
-    pub fn unmap(&self) {
+    fn unmap(&self) {
         unimplemented!()
     }
 }
--- src/kernel_api/objects/vspace/mod.rs
+++ src/kernel_api/objects/vspace/mod.rs
@@ -1,5 +1,38 @@
-mod arm;
-pub use arm::paging::*;
+//! [ASIDPool] and [ASIDControl] for tracking status of an address space.
 
-pub struct VSpace;
+pub mod arm;
+use crate::types::SeL4Error;
+use crate::kernel_api::objects::untyped_memory::Untyped;
+use crate::kernel_api::CapSpace;
+#[cfg(doc)]
+use arm::VSpace;
+
+/// Documentation not present for aarch64
 pub struct IOSpace;
+
+/// Confers the right to create a subset the available applications
+///
+/// For a VSpace to be usable by an app, it must be assigned an ASID using the [ASIDPool::assign] fn
+pub struct ASIDPool;
+
+impl ASIDPool {
+    /// Gives a [VSpace] an id, by way of placing it in an `ASIDPool`
+    ///
+    /// This method must be run before a vspace can be used.
+    pub fn assign(&mut self, vspace: arm::VSpace) -> Result<(), SeL4Error> {
+        unimplemented!()
+    }
+}
+
+/// The capability from which [ASIDPool] capabilities can be created
+pub struct ASIDControl;
+
+impl ASIDControl {
+    pub fn make_pool(
+        &mut self,
+        untyped: Untyped,
+        cspace: &mut CapSpace,
+    ) -> Result<Self, SeL4Error> {
+        unimplemented!()
+    }
+}
